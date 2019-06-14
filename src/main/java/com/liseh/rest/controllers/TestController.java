4132522576ff707d4d8afbb49ab675a7fff8094f
zamiurratul <zamiurratul@gmail.com>
2019-06-15 00:32:22 +0600

    done

---
+++ src/main/java/com/liseh/rest/controllers/TestController.java
@@ -0,0 +1,16 @@
+package com.liseh.rest.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    String getName(){
+        return "Liseh Test";
+    }
+
+}
