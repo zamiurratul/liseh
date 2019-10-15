f6e4c80798c930cf4e767025c6ded9bbaf5f8fb5
Zamiur Rahman Ratul <zamiurratul@gmail.com>
2019-10-15 22:50:34 +0600

    Remove unnecessary modules

--- liseh-bll-producer/src/main/java/com/liseh/bll/producer/RegistrationProducer.java
+++
@@ -1,9 +0,0 @@
-package com.liseh.bll.producer;
-
-import java.util.concurrent.ExecutionException;
-
-public interface RegistrationProducer {
-    //TODO return and method parameter will be changed to DTO
-    String preRegistration(String content) throws InterruptedException, ExecutionException;
-    String registration(String content) throws InterruptedException, ExecutionException;
-}
